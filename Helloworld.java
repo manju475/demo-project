2bed3509177b0c79a47ff7a3386ac288a2cd49e4
root <root@ip-172-31-95-59.ec2.internal>
2023-11-05 02:41:12 +0000

    Revert "deleted file in git"
    This reverts commit 67047c99cd338ac799ea6935b29c98470b17e851.

---
+++ Helloworld.java
@@ -0,0 +1,11 @@
+//this is my first programme
+class First{
+	public static void main(String[] args){
+		System.out.println("First");
+		int a=5;
+		int b=10;
+		System.out.println(int c=a+b);
+		System.out.peintln("code changed for new commit");
+	}
+}
+
