230b81aba05f29f977ab2ecc7dbe1a1344befcc2
root <root@ip-172-31-95-59.ec2.internal>
2023-11-05 10:26:42 +0000

    4th commit

--- Helloworld.java
+++ Helloworld.java
@@ -1,11 +1,14 @@
 //this is my first programme
-class First{
+//
+//
+class Helloworld {
 	public static void main(String[] args){
 		System.out.println("First");
 		int a=5;
 		int b=10;
-		System.out.println(int c=a+b);
-		System.out.peintln("code changed for new commit");
+		int c;
+		System.out.println(c=a+b);
+		System.out.println("code dhanged for new commit");
 	}
 }
 
